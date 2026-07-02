f6801d970c3468dbe1c480d168e16b78191e5bdd
Luisro <luis.ardon@ufm.edu>
2026-07-01 18:26:29 -0600

    Add patient header section
    Includes: name, age and medical history

--- pages/1_Paciente.py
+++ pages/1_Paciente.py
@@ -7,5 +7,17 @@ import logica
 
 st.set_page_config(page_title = "Paciente", page_icon = "👤")
 
-st.title("Panel del Pacinete")
-st.write("Aqui el paciente vera y registrara sus datos (en proceso de desarrollo)")
+# --- Obtenemos los datos del paciente Luis Fernando (ID 1) ---
+paciente = logica.pacientes_db[1]    #Accedemos al diccionario y sacamos al paciente con clave [1]
+
+# --- SECCIÓN 1: Encabezado / indentificación ---
+
+st.title("Panel del Paciente")
+st.header(paciente["nombre"])
+st.write("Edad:", paciente["edad"], "años")
+
+# Mostramos el historial médico recorriendo la lista con un ciclo for
+
+st.subheader("Historial médico")
+for condicion in paciente["historial_medico"]:
+    st.write("-", condicion)
