690ecf9f8bceefa3858a8f36d643aeb4fc7ab000
Mduartec <mariana.duarte@ufm.edu>
2026-07-03 05:39:05 -0600

    Add glucose metrics using logica.py functions

--- pages/2_Doctor.py
+++ pages/2_Doctor.py
@@ -21,3 +21,22 @@ st.write(f"**Edad:** {paciente['edad']} anios")
 st.subheader("Historial medico")
 for condicion in paciente["historial_medico"]:
     st.write("-" + condicion)
+
+st.divider()
+
+#SECCION ": Metricas de glucosa
+# lista de registro de paciente 
+registros = paciente ["registros_glucosa"]
+
+#Los tres calculos vienen de funciones de logica.py
+promedio = logica.calcular_promedio_glucosa(registros)
+maxima = logica.encontrar_glucosa_maxima(registros)
+tendencia = logica.detectar_tendencia(registros)
+
+st.subheader("Metricas de glucosa")
+
+#st.columns; crearemos 3
+col1, col2, col3, = st.columns(3)
+col1.metric("Promedio", f"{promedio} mg/dL")
+col2.metric("Maxima", f"{maxima} mg/dL")
+col3.metric("Tendencia", tendencia)
